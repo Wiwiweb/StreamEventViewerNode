d2a4d84e93347360c832d98837b1f5c92b6b5b20
Wiwiweb <wiwiweb2@gmail.com>
2019-02-18 18:39:24 -0800

    Twitch login implemented

--- app.js
+++ app.js
@@ -1,17 +1,62 @@
 const http = require('http');
-
-const hostname = 'localhost';
-const port = 3000;
-
 const express = require('express');
-const app = express();
-app.set('view engine', 'jade');
+const session = require('express-session');
+const passport = require('passport');
+const twitchStrategy = require("passport-twitch").Strategy;
 
-app.get('/', function(req, res) {
-    res.render('home');
+const home = require('./home');
+
+const PORT = process.env.PORT || 3000;
+const HOSTNAME = process.env.HOSTNAME || 'localhost';
+const SESSION_SECRET = process.env.SESSION_SECRET || 'DjTOYIaTRbKpnF5j6Qu715B6lhUyMRyh';
+const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID;
+const TWITCH_CLIENT_SECRET = process.env.TWITCH_CLIENT_SECRET;
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use(session({secret: SESSION_SECRET}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new twitchStrategy({
+        clientID: TWITCH_CLIENT_ID,
+        clientSecret: TWITCH_CLIENT_SECRET,
+        callbackURL: `http://${HOSTNAME}:${PORT}/login/callback`,
+        scope: "user_read"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        return done(null, profile);
+    }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
 });
 
-app.get('/about', function(req, res) {
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+app.use(express.static('public'));
+
+app.get('/', home);
+
+app.get('/login', passport.authenticate('twitch'));
+
+app.get('/login/callback', passport.authenticate('twitch', {
+        successRedirect: '/',
+        failureRedirect: '/'
+    }
+));
+
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+app.get('/streamer', ensureAuthenticated, function(req, res) {
     res.render('streamer');
 });
 
@@ -24,6 +69,16 @@ app.use(function(req, res) {
 
 server = http.createServer(app);
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(PORT, () => {
+    console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
 });
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.status(401).render('error', {
+        message: 'Unauthorized',
+        error: {}
+    });
+}
---
+++ home.js
@@ -0,0 +1,10 @@
+function home(req, res) {
+    if(req.isAuthenticated()) {
+        res.render('home_logged_in', {username: req.user.displayName});
+    }
+    else {
+        res.render('home_logged_out');
+    }
+}
+
+module.exports = home;
